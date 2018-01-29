549452d476d35eefe30c8654ecdb3fa6f7da420b
foxxoffoxxof <1083898216@qq.com>
2018-01-29 17:31:56 +0800

    test1

---
+++ HalloController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Created by Administrator on 2018/1/25.
+ */
+@RestController
+public class HalloController {
+
+    @RequestMapping("/hallo")
+    public String say()
+    {
+        return "你好";
+    }
+}
